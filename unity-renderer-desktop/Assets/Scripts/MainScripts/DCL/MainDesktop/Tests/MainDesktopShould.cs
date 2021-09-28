5f969ba8e1280aeca59b4c6dd7a130e50018e488
Mateo Miccino <mateomiccino@gmail.com>
2021-09-28 13:20:53 -0300

    temp

---
+++ unity-renderer-desktop/Assets/Scripts/MainScripts/DCL/MainDesktop/Tests/MainDesktopShould.cs
@@ -0,0 +1,35 @@
+using System;
+using NUnit.Framework;
+using System.Collections;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class MainDesktopShould : IntegrationTestSuite
+    {
+        private ParcelScene scene;
+
+        protected override WorldRuntimeContext CreateRuntimeContext()
+        {
+            return DCL.Tests.WorldRuntimeContextFactory.CreateWithCustomMocks
+            (
+                sceneController: new SceneController(),
+                state: new WorldState(),
+                componentFactory: new RuntimeComponentFactory()
+            );
+        }
+
+        [UnitySetUp]
+        protected override IEnumerator SetUp()
+        {
+            yield return base.SetUp();
+            scene = Environment.i.world.sceneController.CreateTestScene() as ParcelScene;
+        }
+
+        [UnityTest]
+        public IEnumerator BeTrue()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
