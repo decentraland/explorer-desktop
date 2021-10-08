7b9517e825514240ada6e1d8259c7f7caff51bb8
Mateo Miccino <mateomiccino@gmail.com>
2021-10-08 14:45:26 -0300

    kinerius magic

--- unity-renderer-desktop/Assets/Scripts/MainScripts/DCL/Controllers/SettingsDesktop/DisplaySettings.cs
+++ unity-renderer-desktop/Assets/Scripts/MainScripts/DCL/Controllers/SettingsDesktop/DisplaySettings.cs
@@ -1,4 +1,5 @@
 using System;
+using UnityEngine;
 
 namespace MainScripts.DCL.Controllers.SettingsDesktop
 {
@@ -11,10 +12,27 @@ namespace MainScripts.DCL.Controllers.SettingsDesktop
     }
 
     [Serializable]
-    public struct DisplaySettings
+    public class DisplaySettings : IEquatable<DisplaySettings>, ICloneable
     {
         public WindowMode windowMode;
         public int resolutionSizeIndex;
         public bool vSync;
+
+        public bool Equals(DisplaySettings other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return windowMode == other.windowMode && resolutionSizeIndex == other.resolutionSizeIndex && vSync == other.vSync;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((DisplaySettings) obj);
+        }
+
+        public object Clone() => MemberwiseClone();
     }
 }
