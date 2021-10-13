33ae081a332f0866562fd5aa492950df5ad65f63
Patricio Guerra <kinerius@gmail.com>
2021-10-13 15:23:15 -0300

    fix: compile issue

--- unity-renderer-desktop/Assets/Scripts/MainScripts/DCL/Controllers/SettingsDesktop/DisplaySettings.cs
+++ unity-renderer-desktop/Assets/Scripts/MainScripts/DCL/Controllers/SettingsDesktop/DisplaySettings.cs
@@ -12,27 +12,10 @@ namespace MainScripts.DCL.Controllers.SettingsDesktop
     }
 
     [Serializable]
-    public class DisplaySettings : IEquatable<DisplaySettings>, ICloneable
+    public struct DisplaySettings
     {
         public WindowMode windowMode;
         public int resolutionSizeIndex;
         public bool vSync;
-
-        public bool Equals(DisplaySettings other)
-        {
-            if (ReferenceEquals(null, other)) return false;
-            if (ReferenceEquals(this, other)) return true;
-            return windowMode == other.windowMode && resolutionSizeIndex == other.resolutionSizeIndex && vSync == other.vSync;
-        }
-
-        public override bool Equals(object obj)
-        {
-            if (ReferenceEquals(null, obj)) return false;
-            if (ReferenceEquals(this, obj)) return true;
-            if (obj.GetType() != this.GetType()) return false;
-            return Equals((DisplaySettings) obj);
-        }
-
-        public object Clone() => MemberwiseClone();
     }
 }
