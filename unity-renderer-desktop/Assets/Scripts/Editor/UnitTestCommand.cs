316ddd03938b4bce024c1ce9ffdc557d3af3f6e1
Mateo Miccino <mateomiccino@gmail.com>
2021-10-16 19:28:10 -0300

    test

---
+++ unity-renderer-desktop/Assets/Scripts/Editor/UnitTestCommand.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+
+static class UnitTestCommand
+{
+    static void ExecuteTests()
+    {
+        Console.WriteLine(":: Executing Tests");
+    }
+}
