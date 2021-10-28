c81f7a4914c496be12b23581c71aab30166d6545
Patricio Guerra <kinerius@gmail.com>
2021-10-28 17:17:01 -0300

    fix: preloading not dissapearing when entering as guest

--- unity-renderer-desktop/Assets/Scripts/MainScripts/DCL/Controllers/HUD/Preloading/PreloadingController.cs
+++ unity-renderer-desktop/Assets/Scripts/MainScripts/DCL/Controllers/HUD/Preloading/PreloadingController.cs
@@ -7,11 +7,13 @@ namespace MainScripts.DCL.Controllers.HUD.Preloading
     {
         private GameObject view;
         private BaseVariable<string> loadingMessage => DataStore.i.HUDs.loadingHUD.message;
+        private BaseVariable<bool> isSignUpFlow => DataStore.i.isSignUpFlow;
 
         public void Initialize()
         {
             view = Object.Instantiate(GetView());
             loadingMessage.OnChange += OnMessageChange;
+            isSignUpFlow.OnChange += SignUpFlowChanged;
         }
 
         private GameObject GetView()
@@ -22,12 +24,21 @@ namespace MainScripts.DCL.Controllers.HUD.Preloading
         private void Dispose()
         {
             loadingMessage.OnChange -= OnMessageChange;
+            isSignUpFlow.OnChange -= SignUpFlowChanged;
             Object.Destroy(view.gameObject);
         }
 
         private void OnMessageChange(string current, string previous)
         {
             if (current.Contains("%"))
+            {
+                Dispose();
+            }
+        }
+
+        private void SignUpFlowChanged(bool current, bool previous)
+        {
+            if (current)
             {
                 Dispose();
             }
