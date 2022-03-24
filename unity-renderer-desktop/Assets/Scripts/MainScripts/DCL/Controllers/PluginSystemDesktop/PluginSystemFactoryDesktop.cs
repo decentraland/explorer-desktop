43b37134b9c21a0e00b136d0d77ff866598bda99
Mateo Kuruk Miccino <mateomiccino@gmail.com>
2022-03-23 22:57:12 -0300

    fix

--- unity-renderer-desktop/Assets/Scripts/MainScripts/DCL/Controllers/PluginSystemDesktop/PluginSystemFactoryDesktop.cs
+++ unity-renderer-desktop/Assets/Scripts/MainScripts/DCL/Controllers/PluginSystemDesktop/PluginSystemFactoryDesktop.cs
@@ -17,7 +17,7 @@ namespace DCL
             pluginSystem.Register(() => new TransactionFeature());
             pluginSystem.Register(() => new PreviewMenuPlugin());
             pluginSystem.Register(() => new SkyboxController());
-            pluginSystem.Register(() => new EmoteAnimationsPlugin(DataStore.i.emotes));
+            pluginSystem.Register(() => new EmoteAnimationsPlugin(DataStore.i.emotes, new EmoteAnimationLoaderFactory(), new WearableItemResolver()));
             pluginSystem.RegisterWithFlag(() => new BuilderInWorldPlugin(), "builder_in_world");
             pluginSystem.RegisterWithFlag(() => new TutorialController(), "tutorial");
             pluginSystem.RegisterWithFlag(() => new PlacesAndEventsFeature(), "explorev2");
