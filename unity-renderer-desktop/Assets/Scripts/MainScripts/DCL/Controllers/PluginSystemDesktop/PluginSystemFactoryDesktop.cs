e27c3d53fa5d72399cdea66c30e9082df27716ef
Mateo Kuruk Miccino <mateomiccino@gmail.com>
2022-03-24 10:24:21 -0300

    chore: update plugins

--- unity-renderer-desktop/Assets/Scripts/MainScripts/DCL/Controllers/PluginSystemDesktop/PluginSystemFactoryDesktop.cs
+++ unity-renderer-desktop/Assets/Scripts/MainScripts/DCL/Controllers/PluginSystemDesktop/PluginSystemFactoryDesktop.cs
@@ -18,10 +18,15 @@ namespace DCL
             pluginSystem.Register(() => new TransactionFeature());
             pluginSystem.Register(() => new PreviewMenuPlugin());
             pluginSystem.Register(() => new SkyboxController());
+            pluginSystem.Register(() => new GotoPanelPlugin());
+            pluginSystem.Register(() => new ExperiencesViewerFeature());
             pluginSystem.Register(() => new EmoteAnimationsPlugin(DataStore.i.emotes, new EmoteAnimationLoaderFactory(), new WearableItemResolver()));
             pluginSystem.RegisterWithFlag(() => new BuilderInWorldPlugin(), "builder_in_world");
             pluginSystem.RegisterWithFlag(() => new TutorialController(), "tutorial");
             pluginSystem.RegisterWithFlag(() => new PlacesAndEventsFeature(), "explorev2");
+            pluginSystem.RegisterWithFlag(() => new SkyboxController(), "procedural_skybox");
+            pluginSystem.RegisterWithFlag(() => new EmotesCustomizationFeature(), "emotes_customization");
+            pluginSystem.Register(() => new EmotesWheelFeature());
 
             pluginSystem.SetFeatureFlagsData(DataStore.i.featureFlags.flags);
 
