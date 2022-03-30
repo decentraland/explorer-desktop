df8ec91d71ca57ae252ed721d813d329d414e92b
Kinerius <kinerius@gmail.com>
2022-03-30 17:19:19 -0300

    Updated to master and fixed code.

--- unity-renderer-desktop/Assets/Scripts/MainScripts/DCL/Controllers/PluginSystemDesktop/PluginSystemFactoryDesktop.cs
+++ unity-renderer-desktop/Assets/Scripts/MainScripts/DCL/Controllers/PluginSystemDesktop/PluginSystemFactoryDesktop.cs
@@ -1,9 +1,9 @@
-using AvatarSystem;
 using DCL.Emotes;
+using DCL.EmotesWheel;
+using DCL.EquippedEmotes;
 using DCL.ExperiencesViewer;
 using DCL.Tutorial;
 using DCL.Skybox;
-using EmotesCustomization;
 
 namespace DCL
 {
@@ -23,12 +23,13 @@ namespace DCL
             pluginSystem.Register(() => new GotoPanelPlugin());
             pluginSystem.Register(() => new ExperiencesViewerFeature());
             pluginSystem.Register(() => new EmoteAnimationsPlugin());
+            pluginSystem.Register(() => new EquippedEmotesInitializerPlugin());
+            pluginSystem.Register(() => new EmotesWheelUIPlugin());
             pluginSystem.RegisterWithFlag(() => new BuilderInWorldPlugin(), "builder_in_world");
             pluginSystem.RegisterWithFlag(() => new TutorialController(), "tutorial");
             pluginSystem.RegisterWithFlag(() => new PlacesAndEventsFeature(), "explorev2");
             pluginSystem.RegisterWithFlag(() => new SkyboxController(), "procedural_skybox");
-            pluginSystem.RegisterWithFlag(() => new EmotesCustomizationFeature(), "emotes_customization");
-            pluginSystem.Register(() => new EmotesWheelFeature());
+
 
             pluginSystem.SetFeatureFlagsData(DataStore.i.featureFlags.flags);
 
