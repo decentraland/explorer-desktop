5299b0e710cd817df04707b8cb5a4dbe179cfd5a
Mateo Miccino <mateomiccino@gmail.com>
2022-04-26 09:55:56 -0300

    refactor: plugin system compatibility for explorer-desktop (#258)
    * refactor: plugin system compatibility for explorer-desktop
    
    * compatibility with refactor of https://github.com/decentraland/unity-renderer/pull/2175

--- unity-renderer-desktop/Assets/Scripts/MainScripts/DCL/Controllers/PluginSystemDesktop/PluginSystemFactoryDesktop.cs
+++ unity-renderer-desktop/Assets/Scripts/MainScripts/DCL/Controllers/PluginSystemDesktop/PluginSystemFactoryDesktop.cs
@@ -12,28 +12,10 @@ namespace DCL
     {
         public static PluginSystem Create()
         {
-            var pluginSystem = new PluginSystem();
+            var pluginSystem = PluginSystemFactory.Create();
 
-            pluginSystem.Register(() => new DebugPluginFeature());
-            pluginSystem.Register(() => new ShortcutsFeature());
-            pluginSystem.Register(() => new ExploreV2FeatureDesktop());
-            pluginSystem.Register(() => new DebugShapesBoundingBoxDisplayer());
-            pluginSystem.Register(() => new TransactionFeature());
-            pluginSystem.Register(() => new PreviewMenuPlugin());
-            pluginSystem.Register(() => new SkyboxController());
-            pluginSystem.Register(() => new GotoPanelPlugin());
-            pluginSystem.Register(() => new ExperiencesViewerFeature());
-            pluginSystem.Register(() => new EmoteAnimationsPlugin());
-            pluginSystem.Register(() => new EquippedEmotesInitializerPlugin());
-            pluginSystem.Register(() => new EmotesWheelUIPlugin());
-            pluginSystem.Register(() => new LogReportPlugin());
-            pluginSystem.Register(() => new NFTShapePlugin());
-            pluginSystem.Register(() => new SpawnPointsDisplayerPlugin());
-            pluginSystem.RegisterWithFlag(() => new BuilderInWorldPlugin(), "builder_in_world");
-            pluginSystem.RegisterWithFlag(() => new TutorialController(), "tutorial");
-            pluginSystem.RegisterWithFlag(() => new PlacesAndEventsFeature(), "explorev2");
-            pluginSystem.RegisterWithFlag(() => new SkyboxController(), "procedural_skybox");
-
+            pluginSystem.Register<ExploreV2FeatureDesktop>(() => new ExploreV2FeatureDesktop());
+            pluginSystem.Register<LogReportPlugin>(() => new LogReportPlugin());
 
             pluginSystem.SetFeatureFlagsData(DataStore.i.featureFlags.flags);
 
