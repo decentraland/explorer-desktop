25d7849b7a76b6072ff4a2daad6528ef2103423a
antivortex <anton.vikharev@decentraland.org>
2022-09-09 18:31:08 +0400

    texture compression settings missing increase

--- unity-renderer-desktop/Assets/Scripts/MainScripts/DCL/ConfigurationDesktop/ConfigurationDesktop.cs
+++ unity-renderer-desktop/Assets/Scripts/MainScripts/DCL/ConfigurationDesktop/ConfigurationDesktop.cs
@@ -2,7 +2,7 @@
 {
     public static class TextureCompressionSettingsDesktop
     {
-        public const int GLTF_TEX_MAX_SIZE_DESKTOP = 512;
-        public const int GENERAL_TEX_MAX_SIZE_DESKTOP = 2048;
+        public const int GLTF_TEX_MAX_SIZE_DESKTOP = 1024;
+        public const int GENERAL_TEX_MAX_SIZE_DESKTOP = 4096;
     }
 }
