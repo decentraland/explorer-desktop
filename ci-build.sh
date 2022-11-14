0f2d865aa9d1b9dcc6d73fa4915d4594cd54149e
Juan Molteni <juanignaciomolteni@gmail.com>
2022-11-14 15:31:33 -0300

    Removed avpro import

--- ci-build.sh
+++ ci-build.sh
@@ -2,39 +2,6 @@
 
 source ci-setup.sh
 
-echo Downloading AVProVideo
-
-echo "${GPG_PRIVATE_KEY_BASE64}" | base64 -d > private.gpg
-gpg --import private.gpg
-curl -L 'https://renderer-artifacts.decentraland.org/artifacts/Custom_AVProVideo_2.6.4_ULTRA.unitypackage.gpg' -o Custom_AVProVideo_2.6.4_ULTRA.unitypackage.gpg
-
-gpg --output Custom_AVProVideo_2.6.4_ULTRA.unitypackage --decrypt Custom_AVProVideo_2.6.4_ULTRA.unitypackage.gpg
-
-echo Finished downloading AVProVideo
-
-echo Begin importing AVProVideo
-
-xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' $UNITY_PATH/Editor/Unity \
-  -quit \
-  -batchmode \
-  -importPackage $(pwd)/Custom_AVProVideo_2.6.4_ULTRA.unitypackage \
-  -projectPath "$PROJECT_PATH"
-  
-echo Ended importing AvProVideo
-
-echo Begin importing license
-
-if [ -z "$DEVELOPERS_UNITY_LICENSE_CONTENT_2020_3_BASE64" ]; then
-  echo 'DEVELOPERS_UNITY_LICENSE_CONTENT_2020_3_BASE64 not present. License won''t be configured'
-else
-  LICENSE=$(echo "${DEVELOPERS_UNITY_LICENSE_CONTENT_2020_3_BASE64}" | base64 -di | tr -d '\r')
-
-  echo "Writing LICENSE to license file /root/.local/share/unity3d/Unity/Unity_lic.ulf"
-  echo "$LICENSE" > /root/.local/share/unity3d/Unity/Unity_lic.ulf
-fi
-
-echo End importing license
-
 echo "Building for $BUILD_TARGET at $PROJECT_PATH"
 
 xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' $UNITY_PATH/Editor/Unity \
