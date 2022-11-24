d89abd2a3ed3ccb66e8fd91c7a3e1929a1978eb3
Vitaly Popuzin <popuzin@gmail.com>
2022-11-24 22:10:01 +0100

    small change to re-run CI for the release

--- unity-renderer-desktop/Assets/Scripts/MainScripts/DCL/Controllers/HUD/Minimap/MinimapHUDControllerDesktop.cs
+++ unity-renderer-desktop/Assets/Scripts/MainScripts/DCL/Controllers/HUD/Minimap/MinimapHUDControllerDesktop.cs
@@ -3,10 +3,8 @@ namespace DCL.Controllers.HUD
 {
     public class MinimapHUDControllerDesktop : MinimapHUDController
     {
-        protected override MinimapHUDView CreateView()
-        {
-            return MinimapHUDViewDesktop.Create(this);
-        }
+        protected override MinimapHUDView CreateView() =>
+            MinimapHUDViewDesktop.Create(this);
 
         public MinimapHUDControllerDesktop(MinimapMetadataController minimapMetadataController, IHomeLocationController locationController,  global::DCL.Environment.Model environment) 
             : base(minimapMetadataController, locationController, environment)
