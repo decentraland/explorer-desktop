0865b3bdd56807ae398e0c90e9966714bacc8320
Juan Molteni <juanignaciomolteni@gmail.com>
2022-12-12 19:08:47 -0300

    Readded ff

--- unity-renderer-desktop/Assets/Scripts/MainScripts/DCL/Components/Video/VideoProviderFactory.cs
+++ unity-renderer-desktop/Assets/Scripts/MainScripts/DCL/Components/Video/VideoProviderFactory.cs
@@ -8,7 +8,7 @@ public class VideoProviderFactory
     public static IVideoPluginWrapper CreateVideoProvider()
     {
 #if AV_PRO_PRESENT
-        if (Application.platform != RuntimePlatform.LinuxPlayer)
+        if (DataStore.i.featureFlags.flags.Get().IsFeatureEnabled("use_avpro_player") && Application.platform != RuntimePlatform.LinuxPlayer)
         {
             return new VideoPluginWrapper_AVPro();
         }
