1418056972c464ee127c740d4c497aafce45fe4d
Vitaly Popuzin <popuzin@gmail.com>
2022-12-28 14:23:17 +0100

    add Disposal and Destroying on Quit

--- unity-renderer-desktop/Assets/Scripts/MainScripts/DCL/Utils/DesktopUtils.cs
+++ unity-renderer-desktop/Assets/Scripts/MainScripts/DCL/Utils/DesktopUtils.cs
@@ -1,4 +1,6 @@
+using System;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 namespace MainScripts.DCL.Utils
 {
@@ -6,13 +8,35 @@ namespace MainScripts.DCL.Utils
     {
         public static void Quit()
         {
-#if UNITY_EDITOR
-            // Application.Quit() does not work in the editor so
-            // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
-            UnityEditor.EditorApplication.isPlaying = false;
-#else
-            Application.Quit();
-#endif
+            Debug.Log("Disposing");
+            bool disposed = false;
+            foreach (var mb in Object.FindObjectsOfType<MonoBehaviour>())
+            {
+                Debug.Log(mb.name);
+
+                if (mb is IDisposable disposable)
+                {
+                    disposable.Dispose();
+                    disposed = true;
+                }
+            }
+
+            if(disposed)
+                return;
+            
+            Debug.Log("Destroying");
+            foreach (var mb in Object.FindObjectsOfType<MonoBehaviour>())
+            {
+                Debug.Log(mb.name);
+                Object.Destroy(mb);
+            }
+// #if UNITY_EDITOR
+//             // Application.Quit() does not work in the editor so
+//             // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+//             UnityEditor.EditorApplication.isPlaying = false;
+// #else
+//             Application.Quit();
+// #endif
         }
     }
 }
