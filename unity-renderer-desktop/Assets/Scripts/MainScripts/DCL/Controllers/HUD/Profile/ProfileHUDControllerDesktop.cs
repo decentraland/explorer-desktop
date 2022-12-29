83b97d2102d1c0edc982c9203768cc017611d365
Juan Ignacio Molteni <juanignaciomolteni@gmail.com>
2022-12-29 13:38:09 -0300

    Release 20221228 (#449)
    DO NOT DO GITHUB ACTIONS!

--- unity-renderer-desktop/Assets/Scripts/MainScripts/DCL/Controllers/HUD/Profile/ProfileHUDControllerDesktop.cs
+++ unity-renderer-desktop/Assets/Scripts/MainScripts/DCL/Controllers/HUD/Profile/ProfileHUDControllerDesktop.cs
@@ -1,4 +1,6 @@
+using System;
 using MainScripts.DCL.Utils;
+using Unity.Collections.LowLevel.Unsafe;
 using UnityEngine;
 
 namespace MainScripts.DCL.Controllers.HUD.Profile
@@ -11,12 +13,19 @@ namespace MainScripts.DCL.Controllers.HUD.Profile
             viewDesktop = (ProfileHUDViewDesktop)view;
             viewDesktop.getButtonSignUp.onClick.RemoveAllListeners();
             viewDesktop.getButtonSignUp.onClick.AddListener(OnExitButtonClick); // When you exit the renderer, you will see the launcher where you can sign up
-            viewDesktop.buttonExit.onClick.AddListener(OnExitButtonClick);
+            viewDesktop.exitButtons.ForEach( e => e.onClick.AddListener(OnExitButtonClick));
         }
 
         private void OnExitButtonClick()
         {
             DesktopUtils.Quit();
+        }
+
+        public new void Dispose ()
+        {
+            base.Dispose();
+            viewDesktop.getButtonSignUp.onClick.RemoveAllListeners(); // When you exit the renderer, you will see the launcher where you can sign up
+            viewDesktop.exitButtons.ForEach( e => e.onClick.RemoveAllListeners());
         }
 
         protected override GameObject GetViewPrefab()
